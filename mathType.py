2bffe12a9348697851f8dea4e4bfde2b597cf83f
LucaS4nt0s <lucasamuel30@gmail.com>
2024-08-17 13:02:12 -0300

    funcao de imprimir na tela atualizada

--- mathType.py
+++ mathType.py
@@ -8,6 +8,13 @@ INICIO DO NOSSO PROJETO DE PROJETO DE EXTENSÃO 2
 # Initialize Pygame
 pygame.init()
 
+# Set up color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
 # Set up the game window
 screen_width = 500
 screen_height = 700
@@ -15,8 +22,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Set up the background
 background_image = pygame.image.load("background.png")
-background_image = pygame.transform.scale(
-    background_image, (screen_width, screen_height))
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
 pygame.display.set_caption("ASMD Game")
 
@@ -33,9 +39,9 @@ enemy_image = pygame.transform.scale(enemy_image, (55, 55))
 player_rect = player_image.get_rect()
 enemy_rect = enemy_image.get_rect()
 
-# def text_objects(text, font):
-#     textSurface = font.render(text, True, (255, 255, 255))
-#     background_image.blit(textSurface, ((screen_width - textSurface.get_width()) // 2, (screen_height - textSurface.get_height()) // 2))
+def imprimirCX(text, font, cor, y): # Função para imprimir texto no centro da tela em x
+    textSurface = font.render(text, True, cor)
+    screen.blit(textSurface, ((screen_width - textSurface.get_width()) // 2, y))
 # Function to spawn a new enemy
 
 
@@ -62,15 +68,14 @@ def game():
     fim_de_jogo = False
     get_pos_enemy_x = spawn_enemy()[0]
     get_pos_enemy_y = spawn_enemy()[1]
-    font = pygame.font.Font('NewAmsterdam-Regular.ttf', 23)
+    font = pygame.font.Font('NewAmsterdam-Regular.ttf', 40)
+    font2 = pygame.font.Font('NewAmsterdam-Regular.ttf', 23)
     while running:
         while fim_de_jogo:
 
-            game_over_text = font.render("Game Over", True, (255, 0, 0))
-            screen.blit(game_over_text, ((screen_width - game_over_text.get_width()) // 2, (screen_height - game_over_text.get_height() - 50) // 2))
-
-            play_again_text = font.render("Aperte ESPACO para jogar novamente ou a tecla S para sair", True, (255, 255, 255))
-            screen.blit(play_again_text, ((screen_width - play_again_text.get_width()) //2, (screen_height - play_again_text.get_height() - 50) // 2 + 50))
+            imprimirCX("Fim de jogo!", font, RED, (screen_height // 2) - 100)
+            imprimirCX("Pressione espaço para jogar novamente ou ESC para sair", font2, WHITE, (screen_height // 2) - 50)
+        
 
             pygame.display.flip()
             # Event handling
@@ -78,28 +83,30 @@ def game():
                 if event.type == pygame.QUIT:
                     running = False
                     fim_de_jogo = False
+                    
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         fim_de_jogo = False
                         game()
-                    elif event.key == pygame.K_s:
+                    elif event.key == pygame.K_ESCAPE:
                         running = False
                         fim_de_jogo = False
                         pygame.quit()
+                        
+                        
         # ...
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
+                
 
         # Draw the background
         screen.blit(background_image, (0, 0))
 
         screen.blit(enemy_image, (get_pos_enemy_x, get_pos_enemy_y))
 
-        screen.blit(
-            player_image, ((screen_width - 55) // 2, screen_height - 60))
+        screen.blit(player_image, ((screen_width - 55) // 2, screen_height - 60))
 
         enemy_rect.x = get_pos_enemy_x
         enemy_rect.y = get_pos_enemy_y
@@ -115,12 +122,12 @@ def game():
         direction_y = player_rect.y - enemy_rect.y
 
         # Normalize the direction vector
-        direction_length = math.sqrt(direction_x ** 2 + direction_y ** 2)
+        direction_length = math.sqrt(direction_x ** 2 + direction_y ** 2) + 0.0001
         direction_x /= direction_length
         direction_y /= direction_length
 
         # Set the enemy speed
-        enemy_speed = 0.02
+        enemy_speed = 0.1
 
         # Update the enemy position based on the direction vector
         get_pos_enemy_x += direction_x * enemy_speed
