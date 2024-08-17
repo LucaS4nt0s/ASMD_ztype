7b2fab4396eac6cab097e39b1d47839b055a51ac
LucaS4nt0s <lucasamuel30@gmail.com>
2024-08-17 16:14:31 -0300

    variavel score adicionada

--- mathType.py
+++ mathType.py
@@ -22,7 +22,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Set up the background
 background_image = pygame.image.load("background.png")
-background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+background_image = pygame.transform.scale(
+    background_image, (screen_width, screen_height))
 
 pygame.display.set_caption("ASMD Game")
 
@@ -30,28 +31,33 @@ pygame.display.set_caption("ASMD Game")
 player_image = pygame.image.load("player.png")
 player_image = pygame.transform.scale(player_image, (55, 55))
 
+
 def spawn_enemy():
     x = random.randint(0, screen_width - 55)
     y = random.randint(0, 100)
 
     return [x, y]
 
-qtd_inimigos = 10 # Define a quantidade de inimigos
+
+qtd_inimigos = [1, 2, 3, 5, 7]  # Define a quantidade de inimigos
 # Set up the enemy characters
 enemy_image = pygame.image.load("enemy.png")
 enemy_image = pygame.transform.scale(enemy_image, (55, 55))
 enemy_img = []
 enemy_rect = []
-for i in range(qtd_inimigos): # Gera 10 inimigos
-    enemy_img.append(enemy_image) # Adiciona a imagem do inimigo na lista de imagens de inimigos
+for i in range(qtd_inimigos[4]):  # Gera 10 inimigos
+    # Adiciona a imagem do inimigo na lista de imagens de inimigos
+    enemy_img.append(enemy_image)
     enemy_rect.append(enemy_image.get_rect())
 # Get the rect of the player and enemy (used for collision detection)
 player_rect = player_image.get_rect()
 
+
 def enemy(x, y, i):
     screen.blit(enemy_img[i], (x, y))
 
-def imprimirCX(text, font, cor, y): # Função para imprimir texto no centro da tela em x
+
+def imprimirCX(text, font, cor, y):  # Função para imprimir texto no centro da tela em x
     textSurface = font.render(text, True, cor)
     screen.blit(textSurface, ((screen_width - textSurface.get_width()) // 2, y))
 # Function to spawn a new enemy
@@ -68,17 +74,19 @@ def collision(player, enemy):
 def game():
     fim_de_jogo = False
     running = True
+    score = 0
     font = pygame.font.Font('NewAmsterdam-Regular.ttf', 40)
     font2 = pygame.font.Font('NewAmsterdam-Regular.ttf', 23)
     get_pos_enemy_x = []
     get_pos_enemy_y = []
-    for i in range(qtd_inimigos): # Gera 10 inimigos
+    for i in range(qtd_inimigos[4]):  # Gera 10 inimigos
         get_pos_enemy_x.append(spawn_enemy()[0])
         get_pos_enemy_y.append(spawn_enemy()[1])
     while running:
         while fim_de_jogo:
             imprimirCX("Fim de jogo!", font, RED, (screen_height // 2) - 100)
-            imprimirCX("Pressione espaço para jogar novamente ou ESC para sair", font2, WHITE, (screen_height // 2) - 50)
+            imprimirCX("Pressione espaço para jogar novamente ou ESC para sair",
+                       font2, WHITE, (screen_height // 2) - 50)
 
             pygame.display.flip()
             # Event handling
@@ -87,7 +95,7 @@ def game():
                     running = False
                     fim_de_jogo = False
                     pygame.quit()
-                    
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         fim_de_jogo = False
@@ -96,40 +104,52 @@ def game():
                         running = False
                         fim_de_jogo = False
                         pygame.quit()
-                        
-                        
+
         # ...
-    
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 fim_de_jogo = False
                 pygame.quit()
-          
 
         # Draw the background
         screen.blit(background_image, (0, 0))
 
-        screen.blit(player_image, ((screen_width - 55) // 2, screen_height - 60))
+        screen.blit(
+            player_image, ((screen_width - 55) // 2, screen_height - 60))
 
         direction_x = []
         direction_y = []
         direction_length = []
 
-        for i in range(qtd_inimigos):
+        qtd_inimigos_spawn = 0
+
+        if score < 3:
+            qtd_inimigos_spawn = 0
+        elif score < 9:
+            qtd_inimigos_spawn = 1
+        elif score < 18:
+            qtd_inimigos_spawn = 2
+        elif score < 32:
+            qtd_inimigos_spawn = 3
+        else:
+            qtd_inimigos_spawn = 4
+
+        for i in range(qtd_inimigos[qtd_inimigos_spawn]):
 
             enemy_rect[i].x = get_pos_enemy_x[i]
             enemy_rect[i].y = get_pos_enemy_y[i]
 
             # Calculate the direction vector from enemy to player
-            
+
             direction_x.append(player_rect.x - enemy_rect[i].x)
             direction_y.append(player_rect.y - enemy_rect[i].y)
 
             # Normalize the direction vector
-            
-            direction_length1 = math.sqrt(direction_x[i] ** 2 + direction_y[i] ** 2) + 0.0001
+
+            direction_length1 = math.sqrt(
+                direction_x[i] ** 2 + direction_y[i] ** 2) + 0.0001
             direction_length.append(direction_length1)
             direction_x[i] /= direction_length[i]
             direction_y[i] /= direction_length[i]
@@ -154,9 +174,7 @@ def game():
 
         pygame.draw.rect(screen, (255, 0, 0), player_rect, 4)
 
-
         # Check for collision
-        
 
         pygame.display.flip()
 
@@ -171,7 +189,8 @@ def start_game():
         screen.blit(background_image, (0, 0))
 
         imprimirCX("ASMD Game", font, WHITE, (screen_height // 2) - 100)
-        imprimirCX("Pressione espaço para começar", font2, WHITE, (screen_height // 2) - 50)
+        imprimirCX("Pressione espaço para começar", font2,
+                   WHITE, (screen_height // 2) - 50)
 
         pygame.display.flip()
 
@@ -184,8 +203,8 @@ def start_game():
                     game()
         pygame.display.flip()
 
+
 start_game()
 
 # Quit the game
 pygame.quit()
-
